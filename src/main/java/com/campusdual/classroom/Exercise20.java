2107f0473231fbdcfbbeb1ff44b4ecacc1b9bf71
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-21 14:29:16 +0100

    Ejercicio 20 completado

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,10 +6,22 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List<Person> listPersonas = new ArrayList<>();
 
+        listPersonas.add(new Person("Maria", "Rodriguez"));
+        listPersonas.add(new Teacher("Alejandro", "Sainz","Educación"));
+        listPersonas.add(new PoliceOfficer("David", "Fernandez","B-99"));
+        listPersonas.add(new Doctor("Oscar", "Torres", "Nefrología e infectología"));
+
+
+        return listPersonas;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
+
+        for (Person p : stringList){
+            p.getDetails();
+        }
 
     }
 
